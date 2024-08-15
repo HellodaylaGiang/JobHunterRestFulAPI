8570456455d653fc74eb738ed37004502212afc2
Bún Đậu <124412911+HellodaylaGiang@users.noreply.github.com>
2024-08-15 21:56:30 +0700

    15/8 #93

--- src/main/java/com/example/demo/domain/Company.java
+++ src/main/java/com/example/demo/domain/Company.java
@@ -1,14 +1,17 @@
 package com.example.demo.domain;
 
 import java.time.Instant;
+import java.util.List;
 
 import com.example.demo.util.SecurityUtil;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -44,6 +47,9 @@ public class Company {
     private String createdBy;
 
     private String updatedBy;
+
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    List<User> users;
 
     // Lưu thông tin người dùng đăng nhập, thời gian khi thao tác create, update
     @PrePersist
